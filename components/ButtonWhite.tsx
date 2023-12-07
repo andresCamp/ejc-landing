9d376581d69b15c5988080aa5a184aca22f099c5
andresCamp <andreslcampos@icloud.com>
2023-12-07 13:42:36 -0600

    hubspot experiment

--- components/ButtonWhite.tsx
+++ components/ButtonWhite.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
 interface ButtonProps {
-    text?: string
+    text?: string;
+    onClick?: () => void;
+    type?: "button" | "submit" | "reset";
+    children?: React.ReactNode; // Add children prop
 }
 
-const Button: React.FC<ButtonProps> = ({text = "Click Me"}) => {
+const Button: React.FC<ButtonProps> = ({text, onClick, type = "button", children}) => {
   return (
-    <div className="flex items-center justify-center ">
-      <button className="relative px-2 py-2 font-bold text-white bg-transparent border-none group overflow-hidden">
+    <div className="flex items-center justify-center">
+      <button 
+        type={type} 
+        className="relative px-2 py-2 font-bold text-white bg-transparent border-none group overflow-hidden"
+        onClick={onClick}
+      >
         <span className="z-10 relative group-hover:text-black transition-colors duration-300 ease-linear font-syne font-thin">{text}</span>
+        {/* {children || <span className="z-10 relative group-hover:text-black transition-colors duration-300 ease-linear font-syne font-thin">{text}</span>} */}
         <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-white group-hover:h-full transition-all duration-300 ease-linear"></span>
       </button>
     </div>
@@ -16,4 +24,25 @@ const Button: React.FC<ButtonProps> = ({text = "Click Me"}) => {
 };
 
 export default Button;
+
+
+
+// import React from 'react';
+
+// interface ButtonProps {
+//     text?: string
+// }
+
+// const Button: React.FC<ButtonProps> = ({text = "Click Me"}) => {
+//   return (
+//     <div className="flex items-center justify-center ">
+//       <button className="relative px-2 py-2 font-bold text-white bg-transparent border-none group overflow-hidden">
+//         <span className="z-10 relative group-hover:text-black transition-colors duration-300 ease-linear font-syne font-thin">{text}</span>
+//         <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-white group-hover:h-full transition-all duration-300 ease-linear"></span>
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Button;
 
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 import ButtonWhite from './ButtonWhite'
 import Image from 'next/image'
 import Arrow from '../public/arrow.svg'
+import  HubspotForm  from './HubspotForm'
 
 const Footer = () => {
   return (
     <footer className='flex flex-col gap-16 bg-secondary text-[#E7E6E2] font-thin py-12 px-24'>
       
 
-      <div className='flex flex-row justify-between'>
+      <div className='flex flex-row justify-between items-start'>
         <div className='flex flex-col gap-6'>
           <h2 className='text-lg'>ESTUDIO JORGE CAMPOS</h2>
           <div className='flex flex-row gap-12 text-xs'>
@@ -35,13 +36,16 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className='flex flex-col items-start'>
-          <h3 className='text-xs font-syne text-[#979693]'>Sign up for our seasonal newsletter</h3>
+        {/* <div className='flex flex-col items-start justify-start'> */}
+          {/* <h3 className='text-xs font-syne text-[#979693]'>Sign up for our seasonal newsletter</h3> */}
           <div>
-            <form></form>
-            <ButtonWhite text='SIGN UP' />
+
+            <HubspotForm />
+
+            {/* <ButtonWhite text='SIGN UP' /> */}
+
           </div>
-        </div>
+        {/* </div> */}
       </div>
 
       <div className='flex flex-col gap-4'>
